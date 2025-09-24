cc694d65fdd5c70953fee6050de745cae1baad80
lxnneak <lienlinneakarlsson@gmail.com>
2025-09-24 14:03:42 +0200

    Add: changed p color and content

--- cool-app/src/App.js
+++ cool-app/src/App.js
@@ -7,9 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is now green</p>
         <a
           className="App-link"
           href="https://reactjs.org"
